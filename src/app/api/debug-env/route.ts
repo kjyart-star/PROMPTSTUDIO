e46ab3b5aca2551cdfa733188a9c0c9a1a32474f
yong <jin@yongui-MacBookPro.local>
2026-07-05 10:05:18 +0900

    clean: remove temporary debug-env route

--- src/app/api/debug-env/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.json({
-    hasServiceKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
-    hasSupabaseUrl: !!process.env.NEXT_PUBLIC_SUPABASE_URL,
-    hasAnonKey: !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  })
-}
